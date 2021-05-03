75b98f47cf278c98ad64be86eb3965e2579202ce
Vinicius Lamb Magalhães <vinicius.magalhaes@interprocess.com.br>
2021-05-03 15:43:38 -0300

    Corrige warnings

--- jogo.c
+++ jogo.c
@@ -5,44 +5,34 @@ int main(void) {
 	struct game_state state;
 	NewGame(&state);
 	MostrarCursor(false);
-
 	int n_placar = ObterPlacar(p);
-
 	Display(state.board, state.score, state.movimentos, p, n_placar);
 
+	printf("\nFaça uma jogada. Use WASD. E para sair.");
 	// Loop do Jogo
-	while (1) {
+	do {
 		char cmd;
 		// Lê comando
-		printf("\nFaça uma jogada. Use WASD. E para sair.");
 		cmd = _getch();
 		if (cmd == 'e') return;
 
 		// Processa comando
 		Jogada(&state, cmd);
 
-		// checar se o jogo acabou
-		if (Jogo_Acabou(state.board)) {
-			printf("Você venceu!");
-			goto SAIR;
-		}
-
 		// Display
 		Display(state.board, state.score, state.movimentos, p, n_placar);
-	}
+	} while (!Jogo_Acabou(state.board));
 
-SAIR:;
 	return 0;
 }
 
 void Jogada(struct game_state* state, char cmd) {
-	Board* b = state->board;
 
 	switch (cmd) {
-	case 'w': Jogada_Cima(b, &state->score);       break;
-	case 'a': Jogada_Esquerda(b, &state->score);   break;
-	case 's': Jogada_Baixo(b, &state->score);      break;
-	case 'd': Jogada_Direita(b, &state->score);    break;
+	case 'w': Jogada_Cima(state->board, &state->score);       break;
+	case 'a': Jogada_Esquerda(state->board, &state->score);   break;
+	case 's': Jogada_Baixo(state->board, &state->score);      break;
+	case 'd': Jogada_Direita(state->board, &state->score);    break;
 	default: return;
 	}
 	state->movimentos++;
